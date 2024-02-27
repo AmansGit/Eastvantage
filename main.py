fc2c4af847b28a3ef6e6b604178d11e699150c27
AmansGit <amans1997a@gmail.com>
2024-02-27 10:05:25 +0530

    implemented DB sqlite3

--- main.py
+++ main.py
@@ -4,9 +4,31 @@ from pydantic import BaseModel
 from fastapi.responses import JSONResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from fastapi.encoders import jsonable_encoder
+import sqlite3
 
 
 app = FastAPI()
+
+
+conn = sqlite3.connect('book_review.db')
+cursor = conn.cursor()
+
+
+# Create tables if they don't exist
+cursor.execute('''CREATE TABLE IF NOT EXISTS books (
+                    id INTEGER PRIMARY KEY,
+                    title TEXT,
+                    author TEXT,
+                    publication_year INTEGER
+                )''')
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS reviews (
+                    id INTEGER PRIMARY KEY,
+                    book_id INTEGER,
+                    review_text TEXT,
+                    rating INTEGER,
+                    FOREIGN KEY(book_id) REFERENCES books(id)
+                )''')
 
 
 # Temporary db 
@@ -27,46 +49,61 @@ class Review(BaseModel):
 
 
 # ENDPOINT to add books
-@app.post("/books/", response_model=Book)
+@app.post("/books/", response_model=Book, tags=["Books"])
 def add_book(book:Book):
-	books_db.append(book)
+	cursor.execute('''INSERT INTO books (title, author, publication_year) 
+                      VALUES (?, ?, ?)''', (book.title, book.author, book.publication_year))
+    conn.commit()
+
+    #it is used to get id of the last row inserted into DB using .execute()
+    book.id = cursor.lastrowid
+	
 	return book
+
+
 
 # ENDPOINT to add reviews
 @app.post("/review/")#, response_model=Review)
 def add_review(review:Review):
-	# book_exists = any(book.book_id == review.book_id for book in books_db)
-	if review.book_id >= len(books_db):
-		raise HTTPException(status_code=404, detail="Book not found")
-	reviews_db.append(review)
+	cursor.execute('''INSERT INTO reviews (book_id, review_text, rating) 
+                      VALUES (?, ?, ?)''', (review.book_id, review.review_text, review.rating))
+
+	conn.commit()
+	review.id = cursor.lastrowid
+
 	return review
 
 
 # ENDPOINT to get books details
 @app.get("/books/", response_model=list[Book])
 def get_books(author: str=None, publication_year:int = None):
-	filtered_book = books_db
+
+	query = "SELECT * FROM books"
 	if author:
-		filtered_book = [book for book in filtered_book if book.author == author]
+        query += f" WHERE author = '{author}'"
 
 	if publication_year:
-		filtered_book = [book for book in filtered_book if book.publication_year == publication_year]
-	else:
-		filtered_book = books_db
+		if author:
+            query += " AND"
+        else:
+			query += " WHERE"
+		query += f" publication_year = '{publication_year}'"
+	cursor.execute(query)
+    books = cursor.fetchall()
 
-	return filtered_book
+	return book
 
 
 # ENDPOINT to get books reviews
 @app.get("/reviews/{book_id}", response_model=list[Review])
 def get_reviews(book_id: int):
-	# if book_id >= len(books_db):
-	# 	raise HTTPException(status_code=404, detail="Book not available")
+	cursor.execute("SELECT * FROM reviews WHERE book_id = ?", (book_id))
+	reviews = cursor.fetchall()
 
-	book_reviews = [review for review in reviews_db if review.book_id == book_id]
-	if not book_reviews:
-		raise HTTPException(status_code=404, detail="No reviews found for the book")
-	return book_reviews
+	if not reviews:
+		raise HTTPException(status_code = 404, details = "Reviews not found")
+	
+	return reviews
 
 
 # Exception Handling
@@ -75,6 +112,6 @@ async def http_exception_handler(request, exc: StarletteHTTPException):
     return JSONResponse(
     		status_code = exc.status_code,
     		content = {
-    			"message":"endpoint not found"
+    			"message": exc.details
     		}
     )
